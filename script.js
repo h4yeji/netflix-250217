d6b934a44107638744387db3c73241d5ef2030c2
h4yeji <h4yeji@gmail.com>
2025-02-18 19:37:33 +0900

    sec commit

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // API
-import { API_KEY } from "./env.js";
+// import { API_KEY } from "./env.js";
 
 const tmdbCommand = "https://api.themoviedb.org/3";
 
 const fetchMovies1 = async () => {
-  const url = `${tmdbCommand}/movie/now_playing?api_key=${API_KEY}&language=ko-kr&page=1`;
+  const url = `${tmdbCommand}/movie/now_playing?api_key=d9aff27da980d723fd407fadc621b9de&language=ko-kr&page=1`;
   const response = await fetch(url);
   const { results } = await response.json();
   return results;
 };
 
 const fetchMovies2 = async () => {
-  const url = `${tmdbCommand}/movie/upcoming?api_key=${API_KEY}&language=ko-kr&page=1`;
+  const url = `${tmdbCommand}/movie/upcoming?api_key=aff27da980d723fd407fadc621b9de&language=ko-kr&page=1`;
   const response = await fetch(url);
   const { results } = await response.json();
   return results;
